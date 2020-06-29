126734ea0ab0b30d1c7bace3ac1f79d8e031f485
Avi-sa <avivish000@gmail.com>
2020-06-29 12:50:27 +0530

    Initial commit

---
+++ AppCal/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'AppCal'
+urlpatterns = [
+    url(r'^index/$', views.index, name='index'),
+    url('calendar/', views.CalendarView.as_view(), name='calendar'),
+    url('event/new/', views.event, name='event_new'),
+	url('event/edit/(?P<event_id>\d+)/', views.event, name='event_edit'),
+]
